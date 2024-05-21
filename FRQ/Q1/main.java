a7a32427ba2dc10be0f458cae58c5485c4565035
katoiyia <156833314+katoiyia@users.noreply.github.com>
2024-05-21 11:50:01 -0400

    Create main.java

---
+++ FRQ/Q1/main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("2024 Q1");
+
+    Feeder f1 = new Feeder(2000);
+    // System.out.println(f1.getcurrentFood());
+    //f1.simulateOneDay(12);
+    //System.out.println(f1.getCurrentFood());
+    System.out.println(f1.simulateManyDays(10, 5))
+
+      } 
+}
